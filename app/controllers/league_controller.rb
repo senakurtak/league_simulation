87f996e81fa13daa545a90929ea2e98a3fade1ed
Sena Kurtak <zskurtak@gmail.com>
2024-06-02 22:55:11 +0300

    chore: indentation fix

--- app/controllers/league_controller.rb
+++ app/controllers/league_controller.rb
@@ -1,14 +1,14 @@
 class LeagueController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:simulate_week, :simulate_all]
 
-# Initialize the league and matches when the controller is created
+  # Initialize the league and matches when the controller is created
   def initialize
     super
     @league_manager = LeagueManager.new
     @match_simulator = MatchSimulator.new(@league_manager)
   end
 
-# Display the initial league table and championship predictions
+  # Display the initial league table and championship predictions
   def index
     session[:current_week] ||= 0
     @statistics_calculator = StatisticsCalculator.new(@league_manager.teams, @league_manager.matches, session[:current_week])
@@ -16,7 +16,7 @@ class LeagueController < ApplicationController
     @championship_predictions = @statistics_calculator.render_championship_predictions
   end
 
-# Simulate matches for one week and update the league table
+  # Simulate matches for one week and update the league table
   def simulate_week
     if session[:current_week] < 8
       session[:current_week] += 1
@@ -32,7 +32,7 @@ class LeagueController < ApplicationController
     }
   end
 
-# Simulate matches for all weeks (8 week) until the end of the season
+  # Simulate matches for all weeks (8 week) until the end of the season
   def simulate_all
     while session[:current_week] < 8
       session[:current_week] += 1
